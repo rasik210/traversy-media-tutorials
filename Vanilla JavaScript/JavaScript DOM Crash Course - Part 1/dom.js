2ab04e98e37bb49dd4b6d825ea8c9f1133324d00
Rasik Bihari <rasik210@gmail.com>
2025-06-18 06:52:11 +0530

    JavaScript DOM Crash Course - Part 2

--- Vanilla JavaScript/JavaScript DOM Crash Course - Part 1/dom.js
+++ Vanilla JavaScript/JavaScript DOM Crash Course - Part 1/dom.js
@@ -104,3 +104,72 @@
 // }
 
 //END: JavaScript DOM Crash Course - Part 1
+
+//START: JavaScript DOM Crash Course - Part 2
+
+var itemList = document.querySelector('#items');
+//parent Node
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+//parent element
+//  console.log(itemList.parentElement);
+//  itemList.parentElement.style.backgroundColor = '#f4f4f4';
+//  console.log(itemList.parentElement.parentElement.parentElement);
+
+//childNodes
+// console.log(itemList.childNodes);
+// console.log(itemList.children);
+// console.log(itemList.children[1]);
+
+// itemList.children[1].style.backgroundColor = 'yellow';
+// //firstchild
+// console.log(itemList.firstChild);
+// //firstelementchild
+// console.log(itemList.firstElementChild);
+
+// itemList.firstElementChild.textContent = 'Hello 1';
+
+// //lastchild
+// console.log(itemList.lastChild);
+// //lastelementchild
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+// //nextSibling
+// console.log(itemList.nextSibling);
+// //nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+// //previousSibling
+// console.log(itemList.previousSibling);
+// //previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+//createElement
+
+// //create a div
+// var newDiv = document.createElement('div');
+// //add CSS class
+// newDiv.className = 'hello';
+// //add id
+// newDiv.id = 'hello1';
+
+// //add attr
+// newDiv.setAttribute('title', 'hello div title');
+
+// //create text node
+// var newDivText = document.createTextNode('Hello World');
+
+// newDiv.appendChild(newDivText);
+
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
+
+// console.log(newDiv);
+
+// newDiv.style.fontSize = '30px';
+// container.insertBefore(newDiv, h1);
+
+//END: JavaScript DOM Crash Course - Part 2
